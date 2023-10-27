1131b43f518f4c1db03f5a0356e237f7690c8cd8
AlexThukynov1 <alexthukynov@gmail.com>
2023-10-27 09:53:47 +0300

    add card and nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { DialogViewComponent } from './components/dialog-view/dialog-view.compon
 import { AppRoutingModule } from './app-routing.module';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -30,7 +32,9 @@ import {MatIconModule} from '@angular/material/icon';
     BrowserAnimationsModule,
     AppRoutingModule,
     MatToolbarModule,
-    MatIconModule
+    MatIconModule,
+    MatButtonModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
